59b605aba6c13806c285dd3fd2e96f24c5c75ada
Yoon-Jae-Min <jmyoon1994@naver.com>
2024-02-20 21:29:37 +0900

    home first

---
+++ Student.java
@@ -0,0 +1,60 @@
+
+public class Student {
+	private String studentID; //학번
+	private String name;  //이름
+	private String major;  //전공
+	private String telNumber;  //전화번호
+	private boolean sex;  //성별 True = 남, False = 여
+	private int grade;  //학년
+	
+	public Student(String name, String major, String telNumber, String studentID, boolean sex, int grade) {
+		this.name = name;
+		this.major = major;
+		this.telNumber = telNumber;
+		this.studentID = studentID;
+		this.sex = sex;
+		this.grade = grade;
+	}
+	
+	public String getStudentID() {
+		return studentID;
+	}
+	
+	public boolean isSex() {
+		return sex;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getMajor() {
+		return major;
+	}
+	
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	
+	public int getGrade() {
+		return grade;
+	}
+	
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	
+	public String getTelNumber() {
+		return telNumber;
+	}
+	
+	public void setTelNumber(String telNumber) {
+		this.telNumber = telNumber;
+	}
+	
+	
+}
---
+++ StudentFunctions.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentFunctions {
+	private ArrayList<Student> st = new ArrayList<Student>();
+	private Scanner scanner = new Scanner(System.in);
+	
+	//등록 기능
+	public void enrollStudent(){
+		
+	}
+	
+	//조회 기능(전체)
+	public void searchList() {
+		
+	}
+	
+	
+	//조회 기능(특정)
+	public void searchEach() {
+		
+	}
+	
+	
+	//삭제기능
+	public void deleteStudent(){
+		
+	}
+	
+	
+	//수정기능
+	public void editStudent() {
+		
+	}
+}
